8b598c498d6a8933f59cf83083dbaf1e46e80426
Gary J <github@accu.ca>
2022-06-14 14:06:39 -0700

    Merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  console.log("Rendering App Component");
+
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState("");
+
+  const onClick = function() {
+    setCounter(counter + 1);
+  };
+
+  const onChange = function(event) {
+    setInput(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const list = [
+    <li>Item 1</li>,
+    <li>Item 2</li>,
+    <li>Item 3</li>,
+    <li>Item 4</li>,
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h2>Hello React</h2>
+
+      <input type="text" value={input} onChange={onChange} />
+      <button onClick={onClick}>Click Me</button>
+      <div>counter = {counter}</div>
+
+      <ul>
+        {list}
+      </ul>
+
+    </div >
   );
 }
 
